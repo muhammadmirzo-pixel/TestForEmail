a25f41e5333d29c894ef433f8c3af66fafe0e686
muhammadmirzo-pixel <140395371+muhammadmirzo-pixel@users.noreply.github.com>
2025-04-27 17:00:35 +0500

    Added custom attribute

---
+++ Attributes/CustomAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace TestForEmail.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method |  AttributeTargets.Property)]
+public class CustomAttribute : Attribute
+{
+    public string Description { get; }
+    public CustomAttribute(string description)
+    {
+        if (string.IsNullOrWhiteSpace(description) || description.Length < 10)
+            throw new ArgumentException("Description must be least 10 characters long");
+        Description = description;
+    }
+}
